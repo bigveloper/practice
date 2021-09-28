1fd11ba51b350607c145dd10f1f035a0f41ec982
bigveloper <bigveloper@gmail.com>
2021-09-29 01:47:50 +0900

    array iteration map test

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,7 +1,11 @@
 const IterationSample = () => {
+    const numbers = [1, 2, 3, 4, 5];
+    const result = numbers.map((num) => num * num);
+    console.log(result);
+
     return (
         <ul>
-            <li>눈사람</li>
+            <li>{result}</li>
             <li>얼음</li>
             <li>눈</li>
             <li>바람</li>
