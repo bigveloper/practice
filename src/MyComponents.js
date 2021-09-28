0d6f4da41bf0c30c42753d03381e77f277f2fbaa
bigveloper <bigveloper@gmail.com>
2021-09-28 21:19:42 +0900

    destructuring assignment

--- src/MyComponents.js
+++ src/MyComponents.js
@@ -1,8 +1,9 @@
 function MyComponents(props) {
+    const { name, children } = props;
     return (
         <div>
-            <h1>안녕하세요 저는 {props.name} 입니다.</h1> <br />
-            <h1>드론이 7대 이면 {props.children}</h1>
+            <h1>안녕하세요 저는 {name} 입니다.</h1> <br />
+            <h1>드런이 7개면 {children}</h1>
         </div>
     );
 }
