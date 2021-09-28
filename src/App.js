372e7b84c5d9dfa261c8740a9914195a8ded263a
bigveloper <bigveloper@gmail.com>
2021-09-29 01:44:53 +0900

    Iteration

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import IterationSample from './IterationSample';
 import EventPractice from './EventPractice';
 import Say from './Say';
 import MyComponents from './MyComponents';
 import './App.css';
 
 function App() {
-    return <EventPractice />;
+    return <IterationSample />;
 }
 
 export default App;
