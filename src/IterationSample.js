84388d7794bdd0cb1d9ecdc25cd3913e8dd96e6f
bigveloper <bigveloper@gmail.com>
2021-09-29 01:51:04 +0900

    array map

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,16 +1,8 @@
 const IterationSample = () => {
-    const numbers = [1, 2, 3, 4, 5];
-    const result = numbers.map((num) => num * num);
-    console.log(result);
+    const names = ['눈사람', '얼음', '눈', '바람'];
+    const nameList = names.map((name) => <li>{name}</li>);
 
-    return (
-        <ul>
-            <li>{result}</li>
-            <li>얼음</li>
-            <li>눈</li>
-            <li>바람</li>
-        </ul>
-    );
+    return <ul>{nameList}</ul>;
 };
 
 export default IterationSample;
