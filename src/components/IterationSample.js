a1ee6804b77442301a11e2c5a5bf52f801c94f75
bigveloper <bigveloper@gmail.com>
2021-09-29 20:22:30 +0900

    -

---
+++ src/components/IterationSample.js
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+const IterationSample = () => {
+    return (
+        <>
+            <input />
+            <button></button>
+        </>
+    );
+};
+
+export default IterationSample;
