b62c78f225e0b038008a641cc1ab138b7de4d19b
bigveloper <bigveloper@gmail.com>
2021-09-29 12:57:26 +0900

    array map

---
+++ src/components/IterationSample.js
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+
+function IterationSample() {
+    const names = ['snowman', 'ice', 'snow', 'wind'];
+    const nameList = names.map((name) => <li>{name}</li>);
+    return <ul>{nameList}</ul>;
+}
+
+export default IterationSample;
