aa1f62391b37f56a8afa2f2874c7a7a5ca1ad1ff
bigveloper <bigveloper@gmail.com>
2021-09-29 10:56:36 +0900

    -

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -9,17 +9,35 @@ function IterationSample() {
         { id: 4, text: 'JavaScript' },
     ]);
 
-    const [input, setInputText] = useState('');
+    const [inputText, setInputText] = useState('');
     const [nextId, setNextId] = useState(5);
     // event
     const onChange = (e) => setInputText(e.target.value);
-
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText,
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    };
+    const onRemove = (id) => {
+        const nextNames = names.filter((name) => name.id !== id);
+        setNames(nextNames);
+    };
+    const nameList = names.map((name) => (
+        <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+            {name.text}
+        </li>
+    ));
     // view
 
     return (
         <div>
-            <input value={value} onChange={onChange} />
-            <button>Add</button>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>Add</button>
+            <ul>{nameList}</ul>
         </div>
     );
 }
