74e76121086aff219e24c08fd9f8059355e7f5d2
bigveloper <bigveloper@gmail.com>
2021-09-29 16:47:35 +0900

    -

--- src/components/IterationSample.js
+++ src/components/IterationSample.js
@@ -1,10 +1,46 @@
 import { useState } from 'react';
 
 function IterationSample() {
-    const names = ['JavaScript', 'React', 'Vue', 'HTML'];
-    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+    // state
+    const [names, setNames] = useState([
+        { id: 1, text: 'JavaScript' },
+        { id: 2, text: 'React' },
+        { id: 3, text: 'Vue' },
+        { id: 4, text: 'HTML' },
+    ]);
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
 
-    return <ul>{nameList}</ul>;
+    // event
+    const onChange = (e) => setInputText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText,
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInputText('');
+    };
+    const onRemove = (id) => {
+        const nextNames = names.filter((name) => name.id !== id);
+        setNames(nextNames);
+    };
+
+    const namesList = names.map((name) => (
+        <li key={name.id} onDoubleClick={() => onRemove(name.id)}>
+            {name.text}
+        </li>
+    ));
+
+    //watch
+    return (
+        <>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>추가</button>
+            <ul>{namesList}</ul>
+        </>
+    );
 }
 
 export default IterationSample;
