ffa494fea80c730b0e6080a55191f9f2f8cc331c
bigveloper <bigveloper@gmail.com>
2021-09-29 13:01:49 +0900

    key={index}

--- src/components/IterationSample.js
+++ src/components/IterationSample.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 function IterationSample() {
     const names = ['snowman', 'ice', 'snow', 'wind'];
-    const nameList = names.map((name) => <li>{name}</li>);
+    const nameList = names.map((name, index) => <li key={index}>{name}</li>);
     return <ul>{nameList}</ul>;
 }
 
