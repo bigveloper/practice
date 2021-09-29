2f3b063b8c7788865ff36a08c177a5dc4caa9f03
bigveloper <bigveloper@gmail.com>
2021-09-29 09:54:49 +0900

    concat, map

--- src/IterationSample.js
+++ src/IterationSample.js
@@ -1,8 +1,44 @@
-const IterationSample = () => {
-    const names = ['눈사람', '얼음', '눈', '바람'];
-    const nameList = names.map((name) => <li>{name}</li>);
+import { useState, useEffect } from 'react';
 
-    return <ul>{nameList}</ul>;
+const IterationSample = () => {
+    // state
+    const [names, setNames] = useState([
+        { id: 1, text: 'snowMan' },
+        { id: 2, text: 'ice' },
+        { id: 3, text: 'snow' },
+        { id: 4, text: 'wind' },
+    ]);
+    const [inputText, setInfutText] = useState('');
+    const [nextId, setNextId] = useState(5);
+
+    // event
+    const onChange = (e) => setInfutText(e.target.value);
+    const onClick = () => {
+        const nextNames = names.concat({
+            id: nextId,
+            text: inputText,
+        });
+        setNextId(nextId + 1);
+        setNames(nextNames);
+        setInfutText('');
+    };
+
+    const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+
+    // watch
+    useEffect(() => {
+        console.log(nameList);
+        console.log(names);
+    }, [nameList, names]);
+
+    // view
+    return (
+        <>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>Add</button>
+            <ul>{nameList}</ul>
+        </>
+    );
 };
 
 export default IterationSample;
