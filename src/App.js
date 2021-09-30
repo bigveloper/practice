5663256eae08c8c5ef45c856e02008b1677f9892
bigveloper <bigveloper@gmail.com>
2021-09-30 21:31:34 +0900

    URL parameter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Link, Route } from 'react-router-dom';
 import About from './About';
 import Home from './Home';
+import Profile from './Profile';
 
 const App = () => {
     return (
@@ -12,10 +13,17 @@ const App = () => {
                 <li>
                     <Link to="/about">소개</Link>
                 </li>
+                <li>
+                    <Link to="/profile/whyj">whyj 프로필</Link>
+                </li>
+                <li>
+                    <Link to="/profile/gildong">gildong 프로필</Link>
+                </li>
             </ul>
             <hr />
             <Route path="/" component={Home} exact={true} />
             <Route path={['/about', '/info']} component={About} />
+            <Route path="/profile/:username" component={Profile} />
         </div>
     );
 };
---
+++ src/Profile.js
@@ -0,0 +1,28 @@
+const data = {
+    whyj: {
+        name: '와재',
+        description: '이것도 고민하고 있네',
+    },
+    gildong: {
+        name: '홍길동',
+        description: '고전 소설 홍길동의 주인공',
+    },
+};
+
+const Profile = ({ match }) => {
+    const { username } = match.params;
+    const profile = data[username];
+    if (!profile) {
+        return <div>존재하지 않는 사용자 입니다.</div>;
+    }
+    return (
+        <div>
+            <h3>
+                {username}({profile.name})
+            </h3>
+            <p>{profile.description}</p>
+        </div>
+    );
+};
+
+export default Profile;
