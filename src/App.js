5b10af7cf4204d162bd440a35eb58616eb8930b5
bigveloper <bigveloper@gmail.com>
2021-10-01 13:44:03 +0900

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import { useState } from 'react';
-import Counter from './Couter';
+import Counter from './Counter';
 import Info from './Info';
 import './App.css';
 
 function App() {
     const [visible, setVisible] = useState(false);
     return (
-        <div>
-            <button
-                onClick={() => {
-                    setVisible(!visible);
-                }}>
-                {visible ? '숨기기' : '보이기'}
-            </button>
-            <hr />
-            {visible && <Info />}
-        </div>
+        <Counter />
+        // <div>
+        //     <button
+        //         onClick={() => {
+        //             setVisible(!visible);
+        //         }}>
+        //         {visible ? '숨기기' : '보이기'}
+        //     </button>
+        //     <hr />
+        //     {visible && <Info />}
+        // </div>
     );
 }
 
