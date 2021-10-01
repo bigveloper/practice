d1302a54dcb75593dad630de6966f5c1fff2572b
bigveloper <bigveloper@gmail.com>
2021-10-01 16:00:23 +0900

    about:detail=true

--- src/About.js
+++ src/About.js
@@ -1,8 +1,16 @@
-function About() {
+import qs from 'qs';
+
+function About({ location }) {
+    const query = qs.parse(location.search, {
+        ignoreQueryPrefix: true,
+    });
+
+    const showDetail = query.detail === 'true';
     return (
         <div>
             <h1>Itroduce</h1>
             <p>this project is React Router Basic learning sample project</p>
+            {showDetail && <p>detail 값을 true 로 선택하셨군요!</p>}
         </div>
     );
 }
