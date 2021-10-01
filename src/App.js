acb96c5afb7a368c10b0b8de92effd8aab50fb1d
bigveloper <bigveloper@gmail.com>
2021-10-01 15:01:58 +0900

    URL Parameter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Profile from './Profile';
 
 function App() {
     return (
@@ -12,10 +13,17 @@ function App() {
                 <li>
                     <Link to="/about">Intorduce</Link>
                 </li>
+                <li>
+                    <Link to="/profile/WhyJ">WhyJ Profile</Link>
+                </li>
+                <li>
+                    <Link to="/profile/YJ">YJ Profile</Link>
+                </li>
             </ul>
             <hr />
             <Route path="/" component={Home} exact={true} />
             <Route path={['/about', '/info']} component={About} />
+            <Route path="/profile/:username" component={Profile} />
         </div>
     );
 }
