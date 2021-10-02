7385f54bfb19c350536d5d51809a5701671e861b
bigveloper <bigveloper@gmail.com>
2021-10-02 16:36:49 +0900

    sampleArticle

--- src/components/NewList.js
+++ src/components/NewList.js
@@ -13,3 +13,26 @@ const NewsListBlock = styled.div`
         padding-right: 1rem;
     }
 `;
+
+const sampleArticle = {
+    title: '제목',
+    description: '내용',
+    url: 'http://google.com',
+    urlToImage: 'https://via.placeholder.com/160',
+};
+
+const NewsList = () => {
+    return (
+        <NewsListBlock>
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+            <NewsItem article={sampleArticle} />
+        </NewsListBlock>
+    );
+};
+
+export default NewsList;
