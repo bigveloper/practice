540c856180e0ae609cf8f6121baa620edec8fb9a
bigveloper <bigveloper@gmail.com>
2021-10-03 22:37:20 +0900

    ===

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import './App.css';
 
 function App() {
-    const name = '날 힘들게 해도';
-    return (
-        <>
-            <h1>리액트 안녕?</h1>
-            <h2>우리 알고지낸지도 한달이 넘었네..??</h2>
-            <h3>너가 {name} 난 너가 좋아</h3>
-            <h4>사실은 내가 널 힘들게 하는 거일 수도...</h4>
-        </>
-    );
+    const name = '리액트';
+    return <div>{name === '리액트' ? <h1>잘해보자</h1> : <h2>못해보자</h2>}</div>;
 }
 
 export default App;
