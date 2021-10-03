3848ba40a1abe69f09d4347b6291a3beefbe646a
bigveloper <bigveloper@gmail.com>
2021-10-03 23:21:32 +0900

    useState, style={{color}}

--- src/Say.js
+++ src/Say.js
@@ -8,11 +8,22 @@ function Say() {
     const onClickEnter = () => setMessage('어서오세요!!');
     const onClickLeave = () => setMessage('어서가세요!!');
 
+    const [color, setColor] = useState('black');
+
     return (
         <div>
             <button onClick={onClickEnter}>입장!!</button>
             <button onClick={onClickLeave}>퇴장!!</button>
-            <h1>{message}</h1>
+            <h1 style={{ color }}>{message}</h1>
+            <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+                RED
+            </button>
+            <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+                RED
+            </button>
+            <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+                RED
+            </button>
         </div>
     );
 }
