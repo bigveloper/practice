4b5d63ea1c3c9cbbf505864f285a1710c550c089
bigveloper <bigveloper@gmail.com>
2021-10-04 00:25:03 +0900

    EventPractice Form

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,17 +2,28 @@ import { useState, useEffect } from 'react';
 
 function EventPractice() {
     // state
-    const [username, setUsername] = useState('');
-    const [message, setMessage] = useState('');
+    const [form, setForm] = useState({
+        username: '',
+        message: '',
+    });
+
+    const { username, message } = form;
 
     // event
-    const onChangeUsername = (e) => setUsername(e.target.value);
-    const onChangeMessage = (e) => setMessage(e.target.value);
+    const onChange = (e) => {
+        const nextForm = {
+            ...form,
+            [e.target.name]: e.target.value,
+        };
+        setForm(nextForm);
+    };
 
     const onClick = () => {
         alert(username + ' : ' + message);
-        setUsername('');
-        setMessage('');
+        setForm({
+            username: '',
+            message: '',
+        });
     };
     const onKeyPress = (e) => {
         if (e.key === 'Enter') {
@@ -28,8 +39,8 @@ function EventPractice() {
     return (
         <div>
             <h1>이벤트 연습입니다.</h1>
-            <input type="text" name="username" value={username} placeholder="사용자명" onChange={onChangeUsername} />
-            <input type="text" name="message" value={message} placeholder="아무거나 입력하세요." onChange={onChangeMessage} onKeyPress={onKeyPress} />
+            <input type="text" name="username" value={username} placeholder="사용자명" onChange={onChange} />
+            <input type="text" name="message" value={message} placeholder="아무거나 입력하세요." onChange={onChange} onKeyPress={onKeyPress} />
             <button onClick={onClick}>확인</button>
         </div>
     );
