cb275993f6d4edc8c6af0cd91ddd1e6451704a75
bigveloper <bigveloper@gmail.com>
2021-10-03 22:38:49 +0900

    &&

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
     const name = '리액트';
-    return <div>{name === '리액트' ? <h1>잘해보자</h1> : <h2>못해보자</h2>}</div>;
+    return <div>{name === '리액트' && <h1>잘해보자</h1>}</div>;
 }
 
 export default App;
