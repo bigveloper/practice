9e55fee5c70b38af8a943da222b7b1d00878c4cf
bigveloper <bigveloper@gmail.com>
2021-10-03 23:02:01 +0900

    Mycomponent

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import MyComponent from './MyComponent';
 
 function App() {
-    const name = undefined;
-    return <>{name}</>;
+    return (
+        <MyComponent name="React" favoriteNumber={0}>
+            리액트
+        </MyComponent>
+    );
 }
 
 export default App;
